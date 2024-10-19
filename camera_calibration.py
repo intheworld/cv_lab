9c42bb1b19f1f72d4b68b18940b5188d3a59ddb9
swliu <liushuwei1991@gmail.com>
2024-10-19 22:15:38 +0800

    [feat] camera calibration

---
+++ camera_calibration.py
@@ -0,0 +1,73 @@
+import os
+
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import pickle
+from typing import cast
+
+output_dir = './output'
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*8,3), np.float32)
+objp[:,:2] = np.mgrid[0:8, 0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d points in real world space
+imgpoints = [] # 2d points in image plane.
+
+# Make a list of calibration images
+images = glob.glob('calibration_wide/GO*.jpg')
+# Step through the list and search for chessboard corners
+for idx, fname in enumerate(images):
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
+
+    # If found, add object points, image points
+    if ret:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (8,6), corners, ret)
+        #write_name = 'corners_found'+str(idx)+'.jpg'
+        #cv2.imwrite(write_name, img)
+        # cv2.imshow('img', img)
+        # cv2.waitKey(500)
+
+# cv2.destroyAllWindows()
+
+
+# Test undistortion on an image
+img = cv2.imread('calibration_wide/test_image.jpg')
+img_size = (img.shape[1], img.shape[0])
+
+# Do camera calibration given object points and image points
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+
+print("camera intrinsic matrix = ", format(mtx))
+
+dst = cv2.undistort(img, mtx, dist, None, mtx)
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+cv2.imwrite("{}/test_undist.jpg".format(output_dir), dst)
+
+# Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+dist_pickle = {}
+dist_pickle["mtx"] = mtx
+dist_pickle["dist"] = dist
+with open('{}/wide_dist_pickle.p'.format(output_dir), "wb") as fw:
+    # noinspection PyTypeChecker
+    pickle.dump(dist_pickle, fw, -1)
+#dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+# Visualize undistortion
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=30)
+ax2.imshow(dst)
+ax2.set_title('Undistorted Image', fontsize=30)
+
+plt.show()
