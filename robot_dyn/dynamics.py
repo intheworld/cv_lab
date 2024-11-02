4d4ee1498ac52e452d9ee5d39eaf37fc082762b2
swliu <liushuwei1991@gmail.com>
2024-11-02 12:18:47 +0800

    [feat] add simple PD control

--- robot_dyn/dynamics.py
+++ robot_dyn/dynamics.py
@@ -76,8 +76,57 @@ for it in range(N_steps):
         viz.display(q)
         time.sleep(20*dt)
 
+# 简单的pid控制
+q = np.zeros(robot.model.nq)
+viz.display(q)
+vq = np.zeros(robot.model.nv)
+# 仿真超参数
+Kf = 0.1
+Kp = 50.
+Kv = 2 * np.sqrt(Kp)
+alpha = 1
+dt = 2e-3
+N_steps = 5000
 
+from utils.traj_ref import TrajRef
 
+qdes = TrajRef(robot.q0, omega = np.array([0.,.1,1,1.5,2.5,-1,-1.5,-2.5,.1,.2,.3,.4,.5,.6]), amplitude = 1.5)
+
+hq    = []   ### For storing the logs of measured trajectory q
+hqdes = []   ### For storing the logs of desired trajectory qdes
+for it in range(N_steps):
+    t = it * dt
+
+    M = pin.crba(robot.model, robot.data, q)
+    b = pin.nle(robot.model, robot.data, q, vq)
+
+    # Compute the force that apply
+    tauq = - Kp * (q - qdes(t)) - Kv * (vq - qdes.velocity(t)) + alpha * qdes.acceleration(t)
+
+    # Use generalized PFD to calculate aq
+    aq = inv(M) @ (tauq - b)
+
+    # Double integration to update vs and q
+    vq += aq * dt
+    q = pin.integrate(robot.model, q, vq * dt)
+
+    # Visualization
+    if it % 20 == 0:
+        viz.display(q)
+        time.sleep(20 * dt)
+
+    # Log the history.
+    hq.append(q.copy())
+    hqdes.append(qdes.copy())
+
+def plot_joint_profiles(i, hq, hqdes):
+    plt.subplot(111)
+    plt.plot([q[i] for q in hq])
+    plt.plot([q[i] for q in hqdes])
+    plt.ylabel(f'Joint {i}')
+    plt.show()
+
+plot_joint_profiles(11, hq, hqdes)
 
 input('Press ENTER to exit.')
 
